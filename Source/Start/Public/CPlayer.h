6a7a6c8683ac837708b07333ed8ba899c9f68bbd
GyungSikHan <gksrudtlr2@icloud.com>
2025-02-19 17:09:09 +0900

    fix

--- Source/Start/Public/CPlayer.h
+++ Source/Start/Public/CPlayer.h
@@ -6,6 +6,7 @@
 #include "Camera/CameraComponent.h"
 #include "CPlayer.generated.h"
 
+class UCWeaponComponent;
 class UCameraComponent;
 class USpringArmComponent;
 class UCMovementComponent;
@@ -25,17 +26,36 @@ protected:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+<<<<<<< Updated upstream
 	//  FPS Ä«¸Þ¶ó & ½ºÇÁ¸µ¾Ï ¼³Á¤
+=======
+	//  ï¿½Ìµï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ô¼ï¿½ (UCMovementComponentï¿½ï¿½ È°ï¿½ï¿½)
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void StartJump();
+	void StopJump();
+	void StartRun();
+	void StopRun();
+
+private:
+	//  FPS Ä«ï¿½Þ¶ï¿½ & ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+>>>>>>> Stashed changes
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	UCameraComponent* Camera;
 
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	USpringArmComponent* SpringArm;
 
+<<<<<<< Updated upstream
 	bool bIsFirstPerson;
 	void ToggleView();
 
 	//  ÀÌµ¿ ÄÄÆ÷³ÍÆ® Ãß°¡
+=======
+	//  ï¿½Ìµï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ® ï¿½ß°ï¿½
+>>>>>>> Stashed changes
 	UPROPERTY(VisibleAnywhere, Category = "Component")
 	UCMovementComponent* MovementComponent;
+	UPROPERTY()
+	UCWeaponComponent* WeaponComponent;
 };
