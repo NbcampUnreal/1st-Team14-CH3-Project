220811cd298c43618df218c8692b8faac3820d1e
raindrovvv <vnrs97@gmail.com>
2025-02-21 00:49:20 +0900

    복구...

--- Source/Start/Public/CPlayer.h
+++ Source/Start/Public/CPlayer.h
@@ -25,7 +25,6 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	UCameraComponent* Camera;
