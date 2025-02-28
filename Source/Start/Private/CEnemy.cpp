469b32b5c0cf3ac753f13fa99848a6c3c501e1b0
swehio <63448248+swehio@users.noreply.github.com>
2025-02-28 21:41:12 +0900

    [fix] attack gap
    1. 보스 근접 공격 구현
    2. 드론과 총 쏜는 적 공격 간격 조정

--- Source/Start/Private/CEnemy.cpp
+++ Source/Start/Private/CEnemy.cpp
@@ -3,18 +3,21 @@
 
 #include "CEnemy.h"
 #include "CEnemyAIController.h"
-#include "Weapon/CWeapon.h"
+#include "CSpawnComponent.h"
 #include "Components/CWeaponComponent.h"
+
+#include "Blueprint/UserWidget.h"
 #include "Weapon/CWeapon.h"
 #include "CGameState.h"
 #include "CCharacter.h"
 #include "CPlayer.h"
+#include "CPlayerController.h"
 #include "Components/CStateComponent.h"
 #include "Components/CMovementComponent.h"
 #include "Components/CStatusComponent.h"
-#include "Components/CWeaponComponent.h"
 #include "Components/WidgetComponent.h"
 #include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
 #include "Kismet/KismetMathLibrary.h"
 
 ACEnemy::ACEnemy()
@@ -26,22 +29,78 @@ ACEnemy::ACEnemy()
 	OverheadHPWidget->SetupAttachment(GetMesh());
 	OverheadHPWidget->SetWidgetSpace(EWidgetSpace::Screen);
 
+	SpawnComp = CreateDefaultSubobject<UCSpawnComponent>(TEXT("SpawnComp"));
+
+	BossHPWidgetClass = nullptr;
+	BossHPWidgetInstance = nullptr;
+
 	bCanAttack = false;
 	bIsGunUsed = false;
 }
 
 void ACEnemy::UpdateOverheadHP()
 {
-	if (!OverheadHPWidget) return;
-
-	UUserWidget* OverheadHPWidgetInstance = OverheadHPWidget->GetUserWidgetObject();
-	if (!OverheadHPWidgetInstance) return;
-
-	if (UProgressBar* HPBar = Cast<UProgressBar>(OverheadHPWidgetInstance->GetWidgetFromName(TEXT("HP"))))
+	if (OverheadHPWidget)
 	{
-		HPBar->SetPercent(StatusComponent->GetHealth() /StatusComponent->GetMaxHealth());
+		UUserWidget* OverheadHPWidgetInstance = OverheadHPWidget->GetUserWidgetObject();
+		if (!OverheadHPWidgetInstance) return;
+
+		if (UProgressBar* HPBar = Cast<UProgressBar>(OverheadHPWidgetInstance->GetWidgetFromName(TEXT("HP"))))
+		{
+			HPBar->SetPercent(StatusComponent->GetHealth() / StatusComponent->GetMaxHealth());
+		}
 	}
 
+	if (BossHPWidgetClass && BossHPWidgetInstance)
+	{
+		if (UProgressBar* HPBar = Cast<UProgressBar>(BossHPWidgetInstance->GetWidgetFromName(TEXT("HPBar"))))
+		{
+			HPBar->SetPercent(StatusComponent->GetHealth() / StatusComponent->GetMaxHealth());
+		}
+		if (UTextBlock* BossName = Cast<UTextBlock>(BossHPWidgetInstance->GetWidgetFromName(TEXT("EnemyName"))))
+		{
+			BossName->SetText(EnemyName);
+		}
+	}
+}
+
+float ACEnemy::GetEnemyHP() const
+{
+	return StatusComponent->GetHealth();
+}
+
+void ACEnemy::VisibleEnemyHPBar()
+{
+	if (BossHPWidgetClass == nullptr)
+	{
+		return;
+	}
+
+	if (!BossHPWidgetInstance)
+	{
+		APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			ACPlayerController* CPlayerController = Cast<ACPlayerController>(PlayerController);
+			if (CPlayerController)
+			{
+				BossHPWidgetInstance = CreateWidget<UUserWidget>(CPlayerController, BossHPWidgetClass);
+				BossHPWidgetInstance->AddToViewport();
+			}
+		}
+
+		UpdateOverheadHP();
+	}
+
+}
+
+void ACEnemy::HiddenEnemyHPBar() 
+{
+	if (BossHPWidgetInstance)
+	{
+		BossHPWidgetInstance->RemoveFromParent();
+		BossHPWidgetInstance = nullptr;
+	}
 }
 
 void ACEnemy::EnemyAttack()
@@ -64,6 +123,11 @@ void ACEnemy::SetEquipMode()
 void ACEnemy::SetActionMode()
 {
 	StateComponent->SetActionMode();
+}
+
+bool ACEnemy::IsEnemyActionMode()
+{
+	return StateComponent->IsActionMode();
 }
 
 void ACEnemy::SetRun()
@@ -118,9 +182,9 @@ void ACEnemy::UnEquip()
 
 void ACEnemy::ToggleAutoFire()
 {
-	if (ACWeapon* Weapon = WeaponComponent->GetCurrentWeapon())
+	if (WeaponComponent->GetCurrentWeapon())
 	{
-		Weapon->ToggleAutoFire();
+		WeaponComponent->ToggleAutoFire();
 		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::Printf(TEXT("Toggle")));
 	}
 		
@@ -136,32 +200,20 @@ void ACEnemy::GunAttackEnd()
 	WeaponComponent->End_Fire();
 }
 
-void ACEnemy::DestroyEnemyInMaze()
-{
-	ACGameState* GameState = GetWorld()->GetGameState<ACGameState>();
-	if (GameState)
-	{
-		EGameState EGS = GameState->GetGameState();
-
-		if (EGS == EGameState::Labyrinth)
-		{
-			Destroy();
-		}
-	}
-}
-
 void ACEnemy::SpawnRandomItemAfterDie()
 {
+	SpawnComp->SpawnRandomActorToPoint(GetMesh()->GetComponentLocation(), FRotator::ZeroRotator);
 	ACWeapon* Weapon = WeaponComponent->GetCurrentWeapon();
 	if (Weapon)
 	{
-		
+		Weapon->FindComponentByClass<USkeletalMeshComponent>()->SetSimulatePhysics(true);
 	}
 }
 
 void ACEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+
 }
 
 void ACEnemy::Tick(float DeltaTime)
@@ -178,7 +230,6 @@ void ACEnemy::Tick(float DeltaTime)
 			HPBarWidgetComponent->SetWorldRotation(UKismetMathLibrary::FindLookAtRotation(HPBarWidgetComponent->GetComponentLocation(), CameraManager->GetCameraLocation()));
 		}
 	}
-
 }
 
 float ACEnemy::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
