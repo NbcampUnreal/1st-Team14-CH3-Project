5db9f34615787cd24f4fbdaec03956f45226287b
swehio <63448248+swehio@users.noreply.github.com>
2025-03-06 01:41:19 +0900

    update boss phase

--- Source/Start/Public/CEnemyProjectile.h
+++ Source/Start/Public/CEnemyProjectile.h
@@ -35,6 +35,8 @@ protected:
 private:
 	FTimerHandle DeleteTimerHandle;
 	UFUNCTION()
-	void OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
 	void DestroyProjectile();
 };
